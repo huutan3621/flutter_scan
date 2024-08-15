22a342fc984b21486934f37da19f1dc43933e813
tanvh <huutan3621@gmail.com>
2024-08-15 11:26:20 +0700

    update ontap unfocus

--- lib/screens/create_item_screen.dart
+++ lib/screens/create_item_screen.dart
@@ -73,190 +73,194 @@ class _CreateItemChildState extends State<CreateItemChild> {
           },
           child: CustomLoadingOverlay(
             isLoading: value.isLoading,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('Cập nhật sản phẩm'),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: value.formKey, // Assign the global key to Form
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          controller: value.itemCodeController,
-                          enabled: value.isItemCodeScanEnabled,
-                          readOnly: true,
-                          decoration: const InputDecoration(labelText: 'SKU'),
-                          onTap: () {
-                            value.scanItemCode(context);
-                          },
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Trường này bắt buộc nhập';
-                            }
-                            return null;
-                          },
-                        ),
-                        TextFormField(
-                          controller: value.barCodeController,
-                          enabled: value.isBarcodeEnable,
-                          readOnly: true,
-                          decoration:
-                              const InputDecoration(labelText: 'Barcode'),
-                          onTap: () {
-                            value.scanBarCode(context);
-                          },
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Trường này bắt buộc nhập';
-                            }
-                            return null;
-                          },
-                        ),
-                        CustomValidateDropDown(
-                          label: "Đơn vị",
-                          selectedItem: value.selectedProductUnit,
-                          itemList: value.unitList,
-                          disabledItemList: value.disableUnitList ?? [],
-                          isRequired: true,
-                          onSelected: (p0) {
-                            value.chooseUnit(p0);
-                          },
-                          validator: (selectedUnit) {
-                            if (value.unitList.isEmpty) {
-                              return 'Không có đơn vị khả dụng';
-                            }
-                            if (selectedUnit == null || selectedUnit.isEmpty) {
-                              return 'Trường này bắt buộc nhập';
-                            }
-                            return null;
-                          },
-                          // isLoading: value.isLoading,
-                        ),
-                        SelectUnitTextFormField(
-                          controller: value.lengthController,
-                          label: "Độ dài",
-                          unit: value.selectedLengthUnit,
-                          selectedUnit: value.selectedLengthUnit,
-                          callback: (previousValue, currentValue) {
-                            value.updateSelectedUnit(
-                                previousValue, currentValue);
-                          },
-                          unitList: value.lengthUnit,
-                          isRequired: true,
-                        ),
-                        SelectUnitTextFormField(
-                          controller: value.widthController,
-                          label: "Chiều rộng",
-                          unit: value.selectedLengthUnit,
-                          selectedUnit: value.selectedLengthUnit,
-                          callback: (previousValue, currentValue) {
-                            value.updateSelectedUnit(
-                                previousValue, currentValue);
-                          },
-                          unitList: value.lengthUnit,
-                          isRequired: true,
-                        ),
-                        SelectUnitTextFormField(
-                          controller: value.heightController,
-                          label: "Chiều cao",
-                          unit: value.selectedLengthUnit,
-                          selectedUnit: value.selectedLengthUnit,
-                          callback: (previousValue, currentValue) {
-                            value.updateSelectedUnit(
-                                previousValue, currentValue);
-                          },
-                          unitList: value.lengthUnit,
-                          isRequired: true,
-                        ),
-                        SelectUnitTextFormField(
-                          controller: value.weightController,
-                          label: "Cân nặng",
-                          unit: value.selectedWeightUnit,
-                          selectedUnit: value.selectedWeightUnit,
-                          callback: (previousValue, currentValue) {
-                            value.updateSelectedWeightUnit(
-                                previousValue, currentValue);
-                          },
-                          unitList: value.weightUnit,
-                        ),
-                        const SizedBox(height: 16),
-                        Wrap(
-                          spacing: 10,
-                          runSpacing: 10,
-                          children: List.generate(
-                            value.images.length,
-                            (index) {
-                              final image = value.images[index];
-                              return Stack(
-                                children: [
-                                  GestureDetector(
-                                    onTap: () {
-                                      value.showImagePreview(context);
-                                    },
-                                    child: SizedBox(
-                                      width:
-                                          (MediaQuery.of(context).size.width /
-                                                  3) -
-                                              20,
-                                      height: 100,
-                                      child: Image.file(
-                                        File(image.path),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned(
-                                    right: 0,
-                                    top: 0,
-                                    child: IconButton(
-                                      icon: const Icon(Icons.cancel,
-                                          color: Colors.white),
-                                      onPressed: () {
-                                        value.removeImage(index);
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              );
+            child: GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Cập nhật sản phẩm'),
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: value.formKey, // Assign the global key to Form
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            controller: value.itemCodeController,
+                            enabled: value.isItemCodeScanEnabled,
+                            readOnly: true,
+                            decoration: const InputDecoration(labelText: 'SKU'),
+                            onTap: () {
+                              value.scanItemCode(context);
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Trường này bắt buộc nhập';
+                              }
+                              return null;
                             },
                           ),
-                        )
-                      ],
+                          TextFormField(
+                            controller: value.barCodeController,
+                            enabled: value.isBarcodeEnable,
+                            readOnly: true,
+                            decoration:
+                                const InputDecoration(labelText: 'Barcode'),
+                            onTap: () {
+                              value.scanBarCode(context);
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Trường này bắt buộc nhập';
+                              }
+                              return null;
+                            },
+                          ),
+                          CustomValidateDropDown(
+                            label: "Đơn vị",
+                            selectedItem: value.selectedProductUnit,
+                            itemList: value.unitList,
+                            disabledItemList: value.disableUnitList ?? [],
+                            isRequired: true,
+                            onSelected: (p0) {
+                              value.chooseUnit(p0);
+                            },
+                            validator: (selectedUnit) {
+                              if (value.unitList.isEmpty) {
+                                return 'Không có đơn vị khả dụng';
+                              }
+                              if (selectedUnit == null ||
+                                  selectedUnit.isEmpty) {
+                                return 'Trường này bắt buộc nhập';
+                              }
+                              return null;
+                            },
+                            // isLoading: value.isLoading,
+                          ),
+                          SelectUnitTextFormField(
+                            controller: value.lengthController,
+                            label: "Độ dài",
+                            unit: value.selectedLengthUnit,
+                            selectedUnit: value.selectedLengthUnit,
+                            callback: (previousValue, currentValue) {
+                              value.updateSelectedUnit(
+                                  previousValue, currentValue);
+                            },
+                            unitList: value.lengthUnit,
+                            isRequired: true,
+                          ),
+                          SelectUnitTextFormField(
+                            controller: value.widthController,
+                            label: "Chiều rộng",
+                            unit: value.selectedLengthUnit,
+                            selectedUnit: value.selectedLengthUnit,
+                            callback: (previousValue, currentValue) {
+                              value.updateSelectedUnit(
+                                  previousValue, currentValue);
+                            },
+                            unitList: value.lengthUnit,
+                            isRequired: true,
+                          ),
+                          SelectUnitTextFormField(
+                            controller: value.heightController,
+                            label: "Chiều cao",
+                            unit: value.selectedLengthUnit,
+                            selectedUnit: value.selectedLengthUnit,
+                            callback: (previousValue, currentValue) {
+                              value.updateSelectedUnit(
+                                  previousValue, currentValue);
+                            },
+                            unitList: value.lengthUnit,
+                            isRequired: true,
+                          ),
+                          SelectUnitTextFormField(
+                            controller: value.weightController,
+                            label: "Cân nặng",
+                            unit: value.selectedWeightUnit,
+                            selectedUnit: value.selectedWeightUnit,
+                            callback: (previousValue, currentValue) {
+                              value.updateSelectedWeightUnit(
+                                  previousValue, currentValue);
+                            },
+                            unitList: value.weightUnit,
+                          ),
+                          const SizedBox(height: 16),
+                          Wrap(
+                            spacing: 10,
+                            runSpacing: 10,
+                            children: List.generate(
+                              value.images.length,
+                              (index) {
+                                final image = value.images[index];
+                                return Stack(
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () {
+                                        value.showImagePreview(context);
+                                      },
+                                      child: SizedBox(
+                                        width:
+                                            (MediaQuery.of(context).size.width /
+                                                    3) -
+                                                20,
+                                        height: 100,
+                                        child: Image.file(
+                                          File(image.path),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      right: 0,
+                                      top: 0,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.cancel,
+                                            color: Colors.white),
+                                        onPressed: () {
+                                          value.removeImage(index);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              bottomNavigationBar: BottomAppBar(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomButton(
-                      onTap: () async {
-                        showModalBottomSheet<void>(
-                          context: context,
-                          backgroundColor: Colors.transparent,
-                          isScrollControlled: true,
-                          builder: (BuildContext context) {
-                            return modalBottomSheet(value, context);
-                          },
-                        );
-                      },
-                      title: 'Thêm ảnh',
-                      btnColor: Colors.green[300],
-                    ),
-                    CustomButton(
-                      onTap: () async {
-                        if (value.formKey.currentState?.validate() ?? false) {
-                          value.onSubmit(context);
-                        }
-                      },
-                      title: 'Cập nhật',
-                    ),
-                  ],
+                bottomNavigationBar: BottomAppBar(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(
+                        onTap: () async {
+                          showModalBottomSheet<void>(
+                            context: context,
+                            backgroundColor: Colors.transparent,
+                            isScrollControlled: true,
+                            builder: (BuildContext context) {
+                              return modalBottomSheet(value, context);
+                            },
+                          );
+                        },
+                        title: 'Thêm ảnh',
+                        btnColor: Colors.green[300],
+                      ),
+                      CustomButton(
+                        onTap: () async {
+                          if (value.formKey.currentState?.validate() ?? false) {
+                            value.onSubmit(context);
+                          }
+                        },
+                        title: 'Cập nhật',
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
